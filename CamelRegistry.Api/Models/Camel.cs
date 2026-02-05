ea8e520a0a184140d2edc0ca4e17ba6a6a4cca6a
postabarnabas <postabarnabas1017@gmail.com>
2026-02-05 20:11:06 +0100

    feat: add camel model

---
+++ CamelRegistry.Api/Models/Camel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CamelRegistry.Api.Models;
+
+public class Camel
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Color { get; set; }
+
+    [Range(1, 2, ErrorMessage = "HumpCount csak 1 vagy 2 lehet.")]
+    public int HumpCount { get; set; }
+
+    public DateTime? LastFed { get; set; }
+}
