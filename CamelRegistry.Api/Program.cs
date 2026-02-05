d84771bd99b427218177114f526f014e7cd2eacd
postabarnabas <postabarnabas1017@gmail.com>
2026-02-05 20:14:50 +0100

    chore: Update Program.cs configuration

--- CamelRegistry.Api/Program.cs
+++ CamelRegistry.Api/Program.cs
@@ -1,6 +1,17 @@
+using CamelRegistry.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CamelDbContext>(options =>
+    options.UseSqlite("Data Source=camels.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CamelDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.Run();
