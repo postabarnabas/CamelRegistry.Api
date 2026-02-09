913eaec49f72ff75e4bb41ca7f68993846cbffc4
postabarnabas <postabarnabas1017@gmail.com>
2026-02-09 22:16:38 +0100

    feat: add new project for testing

---
+++ CamelRegistry.Api.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using CamelRegistry.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace CamelRegistry.Api.Tests;
+
+public class CustomWebApplicationFactory
+    : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<CamelDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<CamelDbContext>(options =>
+            {
+                options.UseSqlite("Data Source=:memory:");
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<CamelDbContext>();
+            db.Database.OpenConnection();
+            db.Database.EnsureCreated();
+        });
+    }
+}
